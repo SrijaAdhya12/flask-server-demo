86d06fbbe359c717e0a6bfeb1ef5ba731975a88b
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-14 20:40:38 +0530

    fix: pyproject.toml

--- server/app/__main__.py
+++ server/app/__main__.py
@@ -2,5 +2,11 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+
+def main():
+    app.run(host="0.0.0.0", port=5000)
     app.run()
+
+
+if __name__ == "__main__":
+    main()
