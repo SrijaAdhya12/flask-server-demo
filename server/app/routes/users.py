fda90c924bd0b0d118e370d7ff55b455b492e471
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-15 16:33:44 +0530

    feat: added post routes

--- server/app/routes/users.py
+++ server/app/routes/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, abort
+from flask import Blueprint, jsonify, request
 from app.models.data import users
 
 users_bp = Blueprint("users", __name__)
@@ -15,3 +15,14 @@ def get_user(user_id):
     if user is None:
         return jsonify({"message": "User not found"}), 404
     return jsonify(user)
+
+
+@users_bp.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    if not data or "name" not in data or "email" not in data or "age" not in data:
+        return jsonify({"message": "Missing data"}), 404
+    new_id = str(max(map(int, users.keys())) + 1)
+    new_user = {"name": data["name"], "email": data["email"], "age": data["age"]}
+    users[new_id] = new_user
+    return jsonify({"id": new_id, "user": new_user}), 201
